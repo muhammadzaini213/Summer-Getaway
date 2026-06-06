c6d0bc7d19562d875dd34bf340f5940f96696312
Blind0Zero <5mb.5sd@gmail.com>
2026-06-06 16:38:08 +0300

    Dialogue system
    Use test_dialogue_here.tscn for inital testing

---
+++ Global/Dialogue/test_npc_script.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+
+@export var dialogue_manager: CanvasLayer 
+@export var json_data_path: String
+
+func interact() -> void:
+	var data = dialogue_manager.load_dialogue_json(json_data_path)
+	dialogue_manager.start_dialogue(data)
