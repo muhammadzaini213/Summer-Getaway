bf368d78e1e4ca4f4e88cdc0f81deb6f273f751b
John Doe <no@thank.you>
2026-06-20 00:40:17 +0530

    Cave entrance fixes

--- Scenes/Worlds/cave_entrance.gd
+++ Scenes/Worlds/cave_entrance.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 
 func _on_interact() -> void:
-
+	GameState.pub_return_position = interactable.user.global_position
 	get_tree().change_scene_to_file(
 		"res://Scenes/Worlds/cave.tscn"
 	)
