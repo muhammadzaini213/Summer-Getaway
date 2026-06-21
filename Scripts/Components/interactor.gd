4c6f107fc8b19acd52babc7d157638474d7eb17f
PizZaDEV777 <136489203+PizZaDEV777@users.noreply.github.com>
2026-06-21 17:50:25 +0530

    Added Interiors Functions & IDs alongside Player Visibilty
    NOTE: Right now, player is being spawned at spawn if you exit the house, that should be fixed next aswell

--- Scripts/Components/interactor.gd
+++ Scripts/Components/interactor.gd
@@ -16,8 +16,9 @@ func _signal_interactable() -> void:
 
 	if interactables.is_empty():
 		return
-
-	get_node("/root/InteractionUI").press_e()
+		
+	if InteractionUI:
+		InteractionUI.press_e()
 
 	for interactable in interactables:
 
@@ -26,11 +27,9 @@ func _signal_interactable() -> void:
 			interactable.user = user
 
 			interactable.start_interaction()
-
-			var ui = get_node_or_null("/root/InteractionUI")
-
-			if ui:
-				ui.hide_prompt()
+			
+			if InteractionUI:
+				InteractionUI.hide_prompt()
 
 			return
 
@@ -44,8 +43,8 @@ func _on_area_entered(area: Area2D) -> void:
 		return
 
 	interactables.append(area)
-
-	get_node("/root/InteractionUI").show_e()
+	if InteractionUI:
+		InteractionUI.show_e()
 
 
 func _on_area_exited(area: Area2D) -> void:
@@ -59,6 +58,6 @@ func _on_area_exited(area: Area2D) -> void:
 	interactables.erase(area)
 
 	if interactables.is_empty():
-
-		get_node("/root/InteractionUI").hide_prompt()
+		if InteractionUI:
+			InteractionUI.hide_prompt()
 		
--- Scripts/Worlds/Levels/island.gd
+++ Scripts/Worlds/Levels/island.gd
@@ -31,35 +31,25 @@ func _ready() -> void:
 
 	day_system.activate_day_system()
 	
-	if GameState.cave_discovered:
+	if GameState.has_island_return_position:
 
-		var cave = get_node_or_null("Cave")
+		$Player.global_position = (
+			GameState.consume_island_return_position()
+		)
 
-		if cave:
+	elif GameState.pub_return_position != Vector2.ZERO:
 
-			cave.visible = true
-
-		var cave_path = get_node_or_null("CavePath")
-
-		if cave_path:
-
-			cave_path.visible = true
-
-		var cave_water = get_node_or_null("CaveWater")
-
-		if cave_water:
-
-			cave_water.visible = false
-
-			cave_water.enabled = false
-	if GameState.pub_return_position != Vector2.ZERO:
-
-		$Player.global_position = GameState.pub_return_position
+		$Player.global_position = (
+			GameState.pub_return_position
+		)
 
 		GameState.pub_return_position = Vector2.ZERO
-	if GameState.cave_return_position != Vector2.ZERO:
 
-		$Player.global_position = GameState.cave_return_position
+	elif GameState.cave_return_position != Vector2.ZERO:
+
+		$Player.global_position = (
+			GameState.cave_return_position
+		)
 
 		GameState.cave_return_position = Vector2.ZERO
 
